370a91e54a8c7ab6638b28d89439dbc1d9276c82
websphereInfotech <wsinfo08317@gmail.com>
2024-04-18 15:10:55 +0530

    new change

--- app/controller/permissions.js
+++ app/controller/permissions.js
@@ -1,20 +1,3 @@
-// const permission = require("../models/permission");
-
-
-// exports.get_all_permissions = async(req,res) => {
-//     try {
-//         const data = await permission.findAll();
-
-//         if (data.length > 0) {
-//             return res.status(200).json({ status:'true',message:'Permission Data Fetch Successfully', data: data});
-//         } else {
-//             return res.status(404).json({ status:'false',message:'Permission Data not Found '});
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({ status:'false', message:"Internal Server Error"});
-//     }
-// }
 const permission = require("../models/permission");
 
 exports.get_all_permissions = async(req,res) => {
@@ -22,13 +5,18 @@ exports.get_all_permissions = async(req,res) => {
         const data = await permission.findAll();
 
         if (data.length > 0) {
+            // Grouping permissions by role and then by resource
             const groupedPermissions = {};
             data.forEach(item => {
-                const key = `${item.role}_${item.resource}`;
-                if (!groupedPermissions[key]) {
-                    groupedPermissions[key] = [];
+                const role = item.role;
+                const resource = item.resource;
+                if (!groupedPermissions[role]) {
+                    groupedPermissions[role] = {};
                 }
-                groupedPermissions[key].push({
+                if (!groupedPermissions[role][resource]) {
+                    groupedPermissions[role][resource] = [];
+                }
+                groupedPermissions[role][resource].push({
                     permissionValue: item.permissionValue,
                     permission: item.permission,
                     createdAt: item.createdAt,
@@ -36,13 +24,19 @@ exports.get_all_permissions = async(req,res) => {
                 });
             });
 
+            // Constructing the response
             const formattedData = [];
-            Object.keys(groupedPermissions).forEach(key => {
-                const [role, resource] = key.split('_');
+            Object.keys(groupedPermissions).forEach(role => {
+                const permissions = [];
+                Object.keys(groupedPermissions[role]).forEach(resource => {
+                    permissions.push({
+                        resource,
+                        permissions: groupedPermissions[role][resource]
+                    });
+                });
                 formattedData.push({
                     role,
-                    resource,
-                    permissions: groupedPermissions[key]
+                    permissions
                 });
             });
 
