8aaf1e89d8c410e040080a99e88125233d545619
websphereInfotech <wsinfo08317@gmail.com>
2024-05-14 12:57:23 +0530

    update proforma invoice

--- app/controller/ProFormaInvoice.js
+++ app/controller/ProFormaInvoice.js
@@ -5,18 +5,31 @@ const product = require("../models/product");
 
 exports.create_ProFormaInvoice = async (req, res) => {
   try {
-    const { ProFormaInvoice_no, date, validtill, customerId,totalIgst,totalSgst,totalMrp,mainTotal,items } = req.body;
+    const {
+      ProFormaInvoice_no,
+      date,
+      validtill,
+      customerId,
+      totalIgst,
+      totalSgst,
+      totalMrp,
+      mainTotal,
+      items,
+    } = req.body;
     const numberOf = await ProFormaInvoice.findOne({
       where: { ProFormaInvoice_no: ProFormaInvoice_no },
     });
     if (numberOf) {
-      return res
-      .status(400)
-      .json({ status: "false", message: "ProForma Invoice Number Already Exists" });
+      return res.status(400).json({
+        status: "false",
+        message: "ProForma Invoice Number Already Exists",
+      });
     }
     const customerData = await customer.findByPk(customerId);
-    if(!customerData) {
-      return res.status(404).json({status:'false', message:'Customer Not Found'});
+    if (!customerData) {
+      return res
+        .status(404)
+        .json({ status: "false", message: "Customer Not Found" });
     }
 
     if (!items || items.length === 0) {
@@ -24,11 +37,13 @@ exports.create_ProFormaInvoice = async (req, res) => {
         .status(400)
         .json({ status: "false", message: "Required Field oF items" });
     }
-  
-    for(const item of items) {
-      const productname = await product.findByPk(item.productId)
-      if(!productname) {
-        return res.status(404).json({ status:'false', message:'Product Not Found'});
+
+    for (const item of items) {
+      const productname = await product.findByPk(item.productId);
+      if (!productname) {
+        return res
+          .status(404)
+          .json({ status: "false", message: "Product Not Found" });
       }
     }
     const createdInvoice = await ProFormaInvoice.create({
@@ -47,7 +62,6 @@ exports.create_ProFormaInvoice = async (req, res) => {
       ...item,
     }));
     await ProFormaInvoiceItem.bulkCreate(addToProduct);
-
 
     const quotationWithItems = await ProFormaInvoice.findOne({
       where: { id: createdInvoice.id },
@@ -69,7 +83,14 @@ exports.create_ProFormaInvoice = async (req, res) => {
 exports.get_all_ProFormaInvoice = async (req, res) => {
   try {
     const allInvoice = await ProFormaInvoice.findAll({
-      include: [{ model: ProFormaInvoiceItem, as: "items", include:[{model:product,as:'product'}] },{model:customer, as:'customer'}],
+      include: [
+        {
+          model: ProFormaInvoiceItem,
+          as: "items",
+          include: [{ model: product, as: "product" }],
+        },
+        { model: customer, as: "customer" },
+      ],
     });
 
     if (!allInvoice) {
@@ -95,7 +116,14 @@ exports.view_ProFormaInvoice = async (req, res) => {
 
     const data = await ProFormaInvoice.findOne({
       where: { id },
-      include: [{ model: ProFormaInvoiceItem, as: "items", include:[{model:product,as:'product'}] },{model:customer, as:'customer'}],
+      include: [
+        {
+          model: ProFormaInvoiceItem,
+          as: "items",
+          include: [{ model: product, as: "product" }],
+        },
+        { model: customer, as: "customer" },
+      ],
     });
     if (!data) {
       return res
@@ -117,7 +145,17 @@ exports.view_ProFormaInvoice = async (req, res) => {
 exports.update_ProFormaInvoice = async (req, res) => {
   try {
     const { id } = req.params;
-    const { ProFormaInvoice_no, date, validtill, customerId, items ,totalIgst,totalSgst,totalMrp,mainTotal} = req.body;
+    const {
+      ProFormaInvoice_no,
+      date,
+      validtill,
+      customerId,
+      items,
+      totalIgst,
+      totalSgst,
+      totalMrp,
+      mainTotal,
+    } = req.body;
 
     const existingInvoice = await ProFormaInvoice.findByPk(id);
 
@@ -128,13 +166,17 @@ exports.update_ProFormaInvoice = async (req, res) => {
       });
     }
     const customerData = await customer.findByPk(customerId);
-    if(!customerData) {
-      return res.status(404).json({status:'false', message:'Customer Not Found'});
+    if (!customerData) {
+      return res
+        .status(404)
+        .json({ status: "false", message: "Customer Not Found" });
     }
-    for(const item of items) {
-      const productname = await product.findByPk(item.productId)
-      if(!productname) {
-        return res.status(404).json({ status:'false', message:'Product Not Found'});
+    for (const item of items) {
+      const productname = await product.findByPk(item.productId);
+      if (!productname) {
+        return res
+          .status(404)
+          .json({ status: "false", message: "Product Not Found" });
       }
     }
     await ProFormaInvoice.update(
@@ -143,7 +185,10 @@ exports.update_ProFormaInvoice = async (req, res) => {
         date,
         validtill,
         customerId,
-        totalIgst,totalSgst,totalMrp,mainTotal
+        totalIgst,
+        totalSgst,
+        totalMrp,
+        mainTotal,
       },
       { where: { id } }
     );
@@ -152,40 +197,45 @@ exports.update_ProFormaInvoice = async (req, res) => {
       where: { InvoiceId: id },
     });
 
-    const updatedProducts = items.map((item) => item.productId && item.rate);
+    const mergedItems = [];
 
-    const itemsToDelete = existingItems.filter(
-      (item) => !updatedProducts.includes(item.productId && item.rate)
-    );
+    items.forEach((item) => {
+      let existingItem = mergedItems.find(
+        (i) => i.productId === item.productId && i.rate === item.rate
+      );
+      if (existingItem) {
+        existingItem.qty += item.qty;
+      } else {
+        mergedItems.push(item);
+      }
+    });
 
-    for (const item of itemsToDelete) {
-      await item.destroy();
-    }
-
-    for (const item of items) {
+    for (const item of mergedItems) {
       const existingItem = existingItems.find(
         (ei) => ei.productId === item.productId && ei.rate === item.rate
       );
- 
-      console.log("item.qty>>>>>>>>>>>>",item.qty);
-      console.log("existingItem.qty>>>>>>>>>>>>",existingItem.qty);
       if (existingItem) {
-        const newQty = item.qty + existingItem.qty;
-
-
-        await existingItem.update({
-          qty: newQty,
-          mrp:item.mrp,
-        });
+        
+        existingItem.qty = item.qty;
+        await existingItem.save();
       } else {
+        
         await ProFormaInvoiceItem.create({
           InvoiceId: id,
-          productId:item.productId,
-          qty : item.qty,
-          rate:item.rate,
-          mrp:item.mrp,
+          productId: item.productId,
+          qty: item.qty,
+          rate: item.rate,
+          mrp: item.mrp,
         });
       }
+    }
+    const updatedProducts = items.map((item) => ({ productId: item.productId, rate: item.rate }));
+    const itemsToDelete = existingItems.filter(
+      (item) => !updatedProducts.some((updatedItem) => updatedItem.productId === item.productId && updatedItem.rate === item.rate)
+    );
+    
+    for (const item of itemsToDelete) {
+      await item.destroy();
     }
     const updatedInvoice = await ProFormaInvoice.findOne({
       where: { id },
@@ -205,6 +255,7 @@ exports.update_ProFormaInvoice = async (req, res) => {
     });
   }
 };
+
 exports.delete_ProFormaInvoice = async (req, res) => {
   try {
     const { id } = req.params;
@@ -216,9 +267,10 @@ exports.delete_ProFormaInvoice = async (req, res) => {
         .status(400)
         .json({ status: "false", message: "ProForma Invoice Item Not Found" });
     }
-    return res
-      .status(200)
-      .json({ status: "true", message: "ProForma Invoice Item Delete Successfully" });
+    return res.status(200).json({
+      status: "true",
+      message: "ProForma Invoice Item Delete Successfully",
+    });
   } catch (error) {
     console.log(error.message);
     return res
