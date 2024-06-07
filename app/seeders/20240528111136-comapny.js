8942caa0f2d7e4e3d67de667d8b39cc08969162a
websphereInfotech <wsinfo08317@gmail.com>
2024-06-07 11:24:48 +0530

    seeder file change

--- app/seeders/20240528111136-comapny.js
+++ app/seeders/20240528111136-comapny.js
@@ -1,80 +1,83 @@
 "use strict";
-
 /** @type {import('sequelize-cli').Migration} */
+
+const {existingData, existingPermission} = require('../util/seeder');
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const company = await queryInterface.rawSelect(
-      "P_companies",
-      {
-        where: { companyname: "Shree Krishna Industry" },
-      },
-      ["id"]
-    );
-    if (!company) {
-      await queryInterface.bulkInsert(
-        "P_companies",
-        [
-          {
-            companyname: "Shree Krishna Industry",
-            gstnumber: "GSTSI1234567890",
-            email: "krishna@example.com",
-            mobileno: 1234567890,
-            address1: "123 first floor",
-            address2: "Mota varachha",
-            pincode: 395006,
-            state: "Gujarat",
-            city: "Surat",
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-        ],
-        {}
-      );
-    }
-    const existingCompanyUser = await queryInterface.rawSelect(
-      "P_companyUsers",
-      {
-        where: { userId: 1, companyId: 1 },
-      },
-      ["id"]
-    );
-    
-    if (!existingCompanyUser) {
-      await queryInterface.bulkInsert(
-        "P_companyUsers",
-        [
-          {
-            userId: 1,
-            companyId: 1,
-            setDefault: true,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-        ],
-        {}
-      );
-    }    
+    // const company = await queryInterface.rawSelect(
+    //   "P_companies",
+    //   {
+    //     where: { companyname: "Shree Krishna Industry" },
+    //   },
+    //   ["id"]
+    // );
+    // if (!company) {
+    //   await queryInterface.bulkInsert(
+    //     "P_companies",
+    //     [
+    //       {
+    //         companyname: "Shree Krishna Industry",
+    //         gstnumber: "GSTSI1234567890",
+    //         email: "krishna@example.com",
+    //         mobileno: 1234567890,
+    //         address1: "123 first floor",
+    //         address2: "Mota varachha",
+    //         pincode: 395006,
+    //         state: "Gujarat",
+    //         city: "Surat",
+    //         createdAt: new Date(),
+    //         updatedAt: new Date(),
+    //       },
+    //     ],
+    //     {}
+    //   );
+    // }
+
+    // // Check if company user already exists
+    // const existingCompanyUser = await queryInterface.rawSelect(
+    //   "P_companyUsers",
+    //   {
+    //     where: { userId: 1, companyId: 1 },
+    //   },
+    //   []
+    // );
+   await existingData();
+   await existingPermission();
+    // if (!existingCompanyUser) {
+    //   await queryInterface.bulkInsert(
+    //     "P_companyUsers",
+    //     [
+    //       {
+    //         userId: 1,
+    //         companyId: 1,
+    //         setDefault: true,
+    //         createdAt: new Date(),
+    //         updatedAt: new Date(),
+    //       },
+    //     ],
+    //     {}
+    //   );
+    // }
   },
 
   async down(queryInterface, Sequelize) {
-    const companyId = await queryInterface.rawSelect(
-      "P_companies",
-      {
-        where: { companyname: "Shree Krishna Industry" },
-      },
-      ["id"]
-    );
+    // const companyId = await queryInterface.rawSelect(
+    //   "P_companies",
+    //   {
+    //     where: { companyname: "Shree Krishna Industry" },
+    //   },
+    //   ["id"]
+    // );
+    // await queryInterface.bulkDelete(
+    //   "P_companyUsers",
+    //   { companyId: companyId },
+    //   {}
+    // );
 
-    await queryInterface.bulkDelete(
-      "P_companyUsers",
-      { companyId: companyId },
-      {}
-    );
-
-    await queryInterface.bulkDelete(
-      "P_companies",
-      { companyname: "Shree Krishna Industry" },
-      {}
-    );
+    // await queryInterface.bulkDelete(
+    //   "P_companies",
+    //   { companyname: "Shree Krishna Industry" },
+    //   {}
+    // );
   },
 };
---
+++ app/util/seeder.js
@@ -0,0 +1,114 @@
+const company = require("../models/company");
+const User = require("../models/user");
+const CompanyUser = require("../models/companyUser");
+const Permissions = require("../models/permission");
+const { permissions } = require('../middleware/permissions');
+
+const existingData = async () => {
+  try {
+    let existingUser = await User.findOne({
+      where: { username: "Vipul Ghelani" },
+    });
+    console.log("existingUser>>>>>>>>>>>>>", existingUser);
+    if (!existingUser) {
+      existingUser = await User.create({
+        username: "Vipul Ghelani",
+        email: "vipul@example.com",
+        mobileno: 9988776655,
+        password:
+          "$2a$10$Mb31UZbFdVgXmEnXadWx4O2VGzguRS0tIw0XX.DbOzNoPzm8Awja6",
+        salary: null,
+        role: "Super Admin",
+      });
+    }
+
+    let existingCompany = await company.findOne({
+      where: { companyname: "Shree Krishna Industry" },
+    });
+    console.log("existingCompany>>>>>>>>>>>>>", existingCompany);
+    if (!existingCompany) {
+      existingCompany = await company.create({
+        companyname: "Shree Krishna Industry",
+        gstnumber: "GSTSI1234567890",
+        email: "krishna@example.com",
+        mobileno: 1234567890,
+        address1: "123 first floor",
+        address2: "Mota varachha",
+        pincode: 395006,
+        state: "Gujarat",
+        city: "Surat",
+      });
+    }
+
+    const companyUserExist = await CompanyUser.findOne({
+      where: { userId: existingUser.id, companyId: existingCompany.id },
+    });
+    if (!companyUserExist) {
+      await CompanyUser.create({
+        userId: existingUser.id,
+        companyId: existingCompany.id,
+        setDefault: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const existingPermission = async () => {
+  try {
+    const allCompany = await company.findAll();
+    for (const comapy of allCompany) {
+      const promises = [];
+
+      for (const resource in permissions) {
+        const rolePermissions = permissions[resource];
+
+        for (const role in rolePermissions) {
+          const permissionsForRole = rolePermissions[role];
+
+          for (const permissionKey in permissionsForRole) {
+            console.log(permissionsForRole, permissionKey,"Permission Key")
+            const permissionValue = permissionsForRole[permissionKey];
+            const type = resource.includes("Cash") ? true : false;
+            console.log({
+              role,
+              resource,
+              companyId: comapy.id,
+              type,
+              permissionKey,
+              permissionValue,
+            });
+            const isPermissionExist = await Permissions.findOne({
+              where: {
+                role: role,
+                resource: resource,
+                companyId: comapy.id,
+                type: type,
+                permission: permissionKey,
+                permissionValue: permissionValue,
+              },
+            });
+            if (!isPermissionExist) {
+              promises.push({
+                role: role,
+                resource: resource,
+                companyId: comapy.id,
+                type: type,
+                permission: permissionKey,
+                permissionValue: permissionValue,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              });
+            }
+          }
+        }
+      }
+      await Permissions.bulkCreate(promises);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { existingData, existingPermission };
