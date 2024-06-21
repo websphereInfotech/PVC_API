9ca103611d7f5541a7a86898a785d2dc650d294c
websphereInfotech <wsinfo08317@gmail.com>
2024-06-21 16:48:15 +0530

    Change router in notification routes

--- app/route/notification.js
+++ app/route/notification.js
@@ -1,8 +1,7 @@
 const {Router} = require('express');
-const router = require("./bom");
 const {view_all_notification} = require("../controller/notification");
 const adminToken = require("../middleware/adminAuth");
-const rotuer = Router()
+const router = Router()
 
 router.get('/view_all_notification', adminToken("Notification:view_all_notification"), view_all_notification)
 
