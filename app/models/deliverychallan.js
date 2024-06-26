d58278f7fa055f554c978bf2e1bbae5b723f7523
websphereInfotech <wsinfo08317@gmail.com>
2024-06-26 10:45:11 +0530

    add createdBy and updateBy field in delivery challan.

--- app/models/deliverychallan.js
+++ app/models/deliverychallan.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/index");
 const customer = require("./customer");
+const User = require("./user");
 
 const deliverychallan = sequelize.define("P_deliverychallan", {
   challanno: {
@@ -18,9 +19,17 @@ const deliverychallan = sequelize.define("P_deliverychallan", {
     type: DataTypes.INTEGER,
     defaultValue:0
   },
+    updatedBy: { type: DataTypes.INTEGER },
+    createdBy: { type: DataTypes.INTEGER }
 });
 
 customer.hasMany(deliverychallan,{ foreignKey:'customerId', onDelete:'CASCADE',as:'DeliveryCustomer'});
 deliverychallan.belongsTo(customer,{ foreignKey:'customerId', onDelete:'CASCADE', as:'DeliveryCustomer'});
+
+User.hasMany(deliverychallan, { foreignKey: "updatedBy", as: "challanUpdateUser" });
+deliverychallan.belongsTo(User, { foreignKey: "updatedBy", as: "challanUpdateUser" });
+
+User.hasMany(deliverychallan, { foreignKey: "createdBy", as: "challanCreateUser" });
+deliverychallan.belongsTo(User, { foreignKey: "createdBy", as: "challanCreateUser" });
 
 module.exports = deliverychallan;
