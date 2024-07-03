51f7f7b394c8c8366aa40a12cf260278b84fddd4
websphereInfotech <wsinfo08317@gmail.com>
2024-07-03 15:10:23 +0530

    add bom routes permission

--- app/route/bom.js
+++ app/route/bom.js
@@ -5,11 +5,10 @@ const {validation} = require("../constant/validate");
 
 const router = Router();
 
-router.post('/create_bom', adminAuth("Bom:create_production"), validation('create_bom'), create_bom);
-router.put('/update_bom/:bomId', adminAuth("Bom:update_production"), validation('update_bom'), update_bom);
-router.get('/view_all_bom', adminAuth("Bom:view_all_production"), view_all_bom);
-router.get('/view_bom/:bomId', adminAuth("Bom:view_production"), view_bom);
-router.delete('/delete_bom/:bomId', adminAuth("Bom:delete_production"), delete_bom);
-
+router.post('/create_bom', adminAuth("Production:create_production"), validation('create_bom'), create_bom);
+router.put('/update_bom/:bomId', adminAuth("Production:update_production"), validation('update_bom'), update_bom);
+router.get('/view_all_bom', adminAuth("Production:view_all_production"), view_all_bom);
+router.get('/view_bom/:bomId', adminAuth("Production:view_production"), view_bom);
+router.delete('/delete_bom/:bomId', adminAuth("Production:delete_production"), delete_bom);
 
 module.exports = router;
