2b5cfd378e0db64712458828ecc09aebaa41609e
keval-techavtar <keval@techavtar.com>
2024-10-23 23:53:07 +0530

    Git workflow test.

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use(logRequestMiddleware);
 
 app.use("/admin", initRoutes);
 
-app.get("/", (req, res) => res.send("Hello World PVC!"));
+app.get("/", (req, res) => res.send("Hello World PVC World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
