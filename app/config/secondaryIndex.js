5ee3e43e6aa14b0e146ab15522934eeb0133d226
dainik-ess <dainik.eternitysoftsols@gmail.com>
2025-03-18 23:55:31 +0530

    add SECONDARY_DB_PORT

--- app/config/secondaryIndex.js
+++ app/config/secondaryIndex.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require("sequelize");
 const { config } = require("dotenv");
 config();
-const { SECONDARY_DB_USERNAME, SECONDARY_DB_NAME, SECONDARY_DB_PASSWORD, SECONDARY_DB_HOST } = process.env;
+const { SECONDARY_DB_USERNAME, SECONDARY_DB_NAME, SECONDARY_DB_PASSWORD, SECONDARY_DB_HOST, SECONDARY_DB_PORT } = process.env;
 
 const sequelize = new Sequelize({
   database: SECONDARY_DB_NAME,
   username: SECONDARY_DB_USERNAME,
   password: SECONDARY_DB_PASSWORD,
   host: SECONDARY_DB_HOST,
+  port: SECONDARY_DB_PORT,
   dialect: 'mysql',
   logging: true
 });
