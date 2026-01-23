930e6f113b8bead6294e2a026573dc752402f453
Denni007 <kukadiyadenish@gmail.com>
2026-01-24 02:42:08 +0530

    `Removed process.exit(1) from error handling in sequelize connection`

--- app/config/index.js
+++ app/config/index.js
@@ -18,7 +18,6 @@ try {
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
-  // process.exit(1);
 }
 
 module.exports = sequelize;
